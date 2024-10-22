d42f84cb7d0c0a1dbee8f0a6d683b50a420a5321
lenovo <ahmedkhaledibrahim20@gmail.com>
2024-10-22 21:27:06 +0300

    async examples

--- src/asyncEx.py
+++ src/asyncEx.py
@@ -3,22 +3,17 @@ import asyncio
 
 async def main():
     print("main function")
-    task1 =  asyncio.create_task(foo("g"))
-    task2 = asyncio.create_task(foo2())
-
-
+    task1 = asyncio.create_task(foo("one",1))
+    print("between")
+    task2 =  asyncio.create_task(foo("two",2))
+    # await task1
+    # await task2
     print("before await")
    
 
-async def foo(text):
-    print("foo1")
-    # await asyncio.sleep(1)
-    print("after sleep")
-
-async def foo2():
-    await asyncio.sleep(2)
-    print("foo2")
-
+async def foo(text:str,delay:int):
+    print(text)
+    await asyncio.sleep(delay)
+    print(f"after {text}")
 
 asyncio.run(main())
-print("outside function await")
